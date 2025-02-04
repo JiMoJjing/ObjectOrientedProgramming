0a50d949b5687ce757e7385d07be63f9dfa16eff
JiMoJjing <qqq5092@gmail.com>
2025-02-04 14:25:34 +0900

    커밋 테스트

--- 2.Time/Main.cpp
+++ 2.Time/Main.cpp
@@ -50,12 +50,12 @@ private:
 
 int main()
 {
-	Time time1 = Time(10);            // 10시 0분 0초.
-	Time time2 = Time(10, 20);        // 10시 20분 0초.
-	Time time3 = Time(10, 20, 30);    // 10시 20분 30초.
+	//Time time1 = Time(10);            // 10시 0분 0초.
+	//Time time2 = Time(10, 20);        // 10시 20분 0초.
+	//Time time3 = Time(10, 20, 30);    // 10시 20분 30초.
 
-	time2.ShowTime();
-	time2.ShowTimeInSeconds();
+	//time2.ShowTime();
+	//time2.ShowTimeInSeconds();
 
 	std::cout << "\n====================\n\n";
 
